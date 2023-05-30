fe9472ed85c19f9f335a90657e3915b71c585b1e
Claribel Diaz <128192485+Clari002@users.noreply.github.com>
2023-05-30 17:53:01 -0300

    Agregar Ejercicio1

---
+++ practico6/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int a;
+
+int b;
+
+a=10;
+
+b=a;
+
+Console.WriteLine("valor de a:"+a);
+
+Console.WriteLine("valor de b:"+b);
+
+//EJERCICIO 1
+int num=0;
+string s = "245";
+
+if (!int.TryParse(s, out num))
+{
+Console.WriteLine("valor ingresado no es un numero valido"); 
+}else{
+    int invertido=0;
+    Console.WriteLine("Valor de num: "+num);
+
+    while(num>0){
+        invertido = invertido * 10 + num % 10;
+        num /= 10;
+    }
+    Console.WriteLine("Valor invertido: "+invertido);
+}
+
