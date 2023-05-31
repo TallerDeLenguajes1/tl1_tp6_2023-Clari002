955b87f0c5f5d91675f6a91401119bd8b5207117
Claribel Diaz <128192485+Clari002@users.noreply.github.com>
2023-05-31 12:17:43 -0300

    Agregar Ejercicio2

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -31,3 +31,56 @@ Console.WriteLine("valor ingresado no es un numero valido");
     Console.WriteLine("Valor invertido: "+invertido);
 }
 
+//EJERCICIO 3
+double N;
+Console.WriteLine();
+Console.WriteLine("Ingrese un numero: ");
+if (!double.TryParse(Console.ReadLine(), out N))
+{
+    Console.WriteLine("El valor ingresado no es valido");
+    return;
+}else{
+    Console.WriteLine("Valor ingresado: "+N);
+    Console.WriteLine("RESULTADOS");
+    //Valor absoluto
+    double valorAbsoluto = Math.Abs(N);
+    Console.WriteLine("Valor Absoluto: "+valorAbsoluto);
+    //Cuadrado
+    double cuadrado = Math.Pow(N, 2);
+    Console.WriteLine("Cuadrado: "+cuadrado);
+    //Raiz cuadrada
+    double raizCuadrada = Math.Sqrt(N);
+    Console.WriteLine("Raiz cuadrad: "+raizCuadrada);
+    //Seno
+    double seno = Math.Sin(N);
+    Console.WriteLine("Seno: "+seno);
+    //Conseno
+    double coseno = Math.Cos(N);
+    Console.WriteLine("Coseno: "+coseno);
+    //Parte entera de un tipo float
+    double parteEntera = Math.Truncate(N);
+    Console.WriteLine("Parte entera: "+parteEntera);
+}
+
+Console.WriteLine();
+Console.WriteLine("Ingrese primer numero: ");
+if (!double.TryParse(Console.ReadLine(), out double N1))
+{
+     Console.WriteLine("El valor ingresado no es valido");
+    return;
+}
+Console.WriteLine("Ingrese segundo numero: ");
+if (!double.TryParse(Console.ReadLine(), out double N2))
+{
+     Console.WriteLine("El valor ingresado no es valido");
+    return;
+}
+
+double maximo = Math.Max(N1,N2);
+Console.WriteLine("El maximo es: "+maximo);
+double minimo = Math.Min(N1,N2);
+Console.WriteLine("El minimo es: "+minimo);
+
+
+
+
