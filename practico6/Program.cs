f78cd453a854a6771c578336fc8fa6b787b21f5a
Claribel Diaz <128192485+Clari002@users.noreply.github.com>
2023-05-30 21:30:48 -0300

    Agregar Ejercicio2

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -16,7 +16,6 @@ Console.WriteLine("valor de b:"+b);
 //EJERCICIO 1
 int num=0;
 string s = "245";
-
 if (!int.TryParse(s, out num))
 {
 Console.WriteLine("valor ingresado no es un numero valido"); 
@@ -30,4 +29,62 @@ Console.WriteLine("valor ingresado no es un numero valido");
     }
     Console.WriteLine("Valor invertido: "+invertido);
 }
+
+//EJERCICIO 2
+int valor=0, num1, num2, suma, resta, mult, divi;
+Console.WriteLine("(1)Suma");
+Console.WriteLine("(2)Resta");
+Console.WriteLine("(3)Multiplicar");
+Console.WriteLine("(4)Dividir");
+Console.WriteLine("Ingrese un valor: ");
+if (!int.TryParse(Console.ReadLine(), out valor))
+{
+    Console.WriteLine("El valor ingresado no es valido");
+    return;
+}else{
+Console.WriteLine("Valor ingresado: "+valor);
+}
+
+Console.WriteLine("\nPrimer numero: ");
+if (!int.TryParse(Console.ReadLine(),out num1))
+{
+    Console.WriteLine("El valor ingresado no es valido");
+    return; 
+}else{
+    Console.WriteLine("Segundo numero: ");
+    if (!int.TryParse(Console.ReadLine(),out num2))
+    {
+        Console.WriteLine("El valor ingresado no es valido");
+       return;
+    }else{
+        switch (valor)
+        {
+            case 1:
+            suma = num1 + num2;
+            Console.WriteLine("Resultado: "+suma);
+            break;
+
+            case 2:
+            resta = num1 - num2;
+            Console.WriteLine("Resultado: "+resta);
+            break;
+
+            case 3:
+            mult = num1 * num2;
+            Console.WriteLine("Resultado: "+mult);
+            break;
+
+            case 4:
+            divi = num1 / num2;
+            Console.WriteLine("Resultado: "+divi);
+            break;
+
+            default:
+
+            break;
+        }
+    }
+}
+
+
 
